13a677af4a50e3e9e8427b6dafadd8804398e32e
alex1393 <jjbumers@gmail.com>
2022-12-23 23:57:23 -0500

    toggle state

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -7,13 +7,17 @@ export default class TodoListItem extends Component{
         important: false
     }
     onLabelClick = () => {
-        this.setState({
-            done: true
+        this.setState(({done}) => {
+            return{
+                done: !done
+            }
         })
     }
     onMarkImportant = () => {
-        this.setState({
-            important: true
+        this.setState(({important}) => {
+            return{
+                important: !important
+            }
         })
     }
     render(){
