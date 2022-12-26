aa319238ff9f1e701f4a43237e81f205edb9d1a4
alex1393 <jjbumers@gmail.com>
2022-12-25 20:11:30 -0500

    delete state

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,30 +1,47 @@
-import React from "react";
+import React, {Component} from "react";
 import AppHeader from "../app-header/app-header";
 import SearchPanel from "../search-panel/search-panel";
 import ItemStatusFilter from "../item-status-filter/item-status-filter";
 import TodoList from "../todo-list/todo-list";
 import './app.css'
 
-const App = () => {
-    const todoData = [
-        { label: 'Drink coffee', id: 1 },
-        { label: 'Make Awesome App', id: 2 },
-        { label: 'Have a lunch', id: 3 },
-    ]
+export default class App extends Component{
+    constructor() {
+        super();
+    }
 
-    return(
-        <div className='todo-app'>
-            <AppHeader toDo={1} done={3}/>
-            <div className='top-panel d-flex'>
-                <SearchPanel />
-                <ItemStatusFilter />
+    state =  {
+        todoData: [
+            { label: 'Drink coffee', id: 1 },
+            { label: 'Make Awesome App', id: 2 },
+            { label: 'Have a lunch', id: 3 },
+        ]
+    }
+
+    deleteItem = (id) => {
+        let newTodoData = [];
+        this.setState(() => {
+            this.state.todoData.map((item) => {
+                if(item.id != id) newTodoData.push(item)
+            })
+            return {
+                todoData: newTodoData
+            };
+        })
+    }
+    render(){
+        return(
+            <div className='todo-app'>
+                <AppHeader toDo={1} done={3}/>
+                <div className='top-panel d-flex'>
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
+                <TodoList
+                    todos={this.state.todoData}
+                    onDeleted={(id) => this.deleteItem(id)}
+                />
             </div>
-            <TodoList
-                todos={todoData}
-                onDeleted={(id) => console.log('del', id)}
-            />
-        </div>
-    )
+        )
+    }
 }
-
-export default App;
